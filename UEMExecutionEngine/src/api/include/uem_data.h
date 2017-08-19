dc56a23e85e613115b9d5400505544520f031045
chjej202 <chjej202@iris.snu.ac.kr>
2017-08-19 20:55:51 +0900

    Structures for translated codes are added.
    Signed-off-by: chjej202 <chjej202@iris.snu.ac.kr>

--- UEMExecutionEngine/src/api/include/uem_data.h
+++ UEMExecutionEngine/src/api/include/uem_data.h
@@ -27,6 +27,16 @@ typedef enum _ERunCondition {
 	RUN_CONDITION_CONTROL_DRIVEN,
 } ERunCondition;
 
+typedef enum _EPortType {
+	PORT_TYPE_QUEUE,
+	PORT_TYPE_BUFFER,
+} EPortType;
+
+typedef enum _EPortMapType {
+	PORTMAP_TYPE_DISTRIBUTING,
+	PORTMAP_TYPE_BUFFER,
+} EPortMapType;
+
 typedef enum _ETimeMetric {
 	TIME_METRIC_CYCLE,
 	TIME_METRIC_COUNT,
@@ -37,38 +47,107 @@ typedef enum _ETimeMetric {
 	TIME_METRIC_HOUR,
 } ETimeMetric;
 
+
+typedef enum _EPortDirection {
+	PORT_DIRECTION_OUTPUT,
+	PORT_DIRECTION_INPUT,
+} EPortDirection;
+
+
 typedef void (*FnUemTaskInit)(int nTaskId);
 typedef void (*FnUemTaskGo)();
 typedef void (*FnUemTaskWrapup)();
 
+
+typedef uem_bool (*FnTaskModeTranstion)();
+
 typedef struct _STask STask;
+
+typedef struct _SModeMap {
+	int nModeId;
+	char *pszModeName;
+} SModeMap;
+
+
+typedef struct _SVariableIntMap {
+	int nVariableId;
+	char *pszVariableName;
+	int nValue;
+} SVariableIntMap;
+
+
+typedef struct _SModeTransitionMachine {
+	int nTaskId;
+	SModeMap *astModeMap;
+	SVariableIntMap *astVarIntMap;
+	FnTaskModeTranstion fnTransition;
+} SModeTransitionMachine;
 
 typedef struct _STaskGraph {
 	STask *astTasks;
+	STask *pstParentTask;
 } STaskGraph;
 
+
 typedef struct _STask {
-	int nTaskIndex;
+	int nTaskId;
 	const char *pszTaskName;
 	ETaskType enType;
 	FnUemTaskInit fnInit;
 	FnUemTaskGo fnGo;
 	FnUemTaskWrapup fnWrapup;
 	ERunCondition enRunCondition;
-	HThreadMutex hMutex;
-	HThreadEvent hEvent;
 	int nRunRate;
 	int nPeriod;
 	ETimeMetric enPeriodMetric;
 	int nThreadNum;
 	STaskGraph *pstSubGraph;
+	STaskGraph *pstParentGraph;
+	SModeTransitionMachine *pstMTMInfo;
+	HThreadMutex hMutex;
+	HThreadEvent hEvent;
 } STask;
 
-typedef struct _SModeTransitionMachine {
+typedef struct _SChunk {
+	void *pChunkStart;
+	void *pDataStart;
+	void *pDataEnd;
+	int nChunkDataLen;
+	HThreadMutex hChunkMutex; // chunk mutex
+	HThreadEvent hChunkEvent; // chunk event for blocking/read
+} SChunk;
 
-} SModeTransitionMachine;
+typedef struct _SAvailableChunk SAvailableChunk;
+
+typedef struct _SAvailableChunk {
+	int nChunkIndex;
+	SAvailableChunk *pstPrev;
+	SAvailableChunk *pstNext;
+} SAvailableChunk;
 
 
+typedef struct _SPort {
+	int nTaskId;
+	char *pszPortName;
+	int nSampleSize;
+	EPortType enPortType;
+	EPortMapType enPortMapType;
+	SChunk *astChunk;
+	int nChunkNum; // loop count number, all channel has at least one chunk
+	int nChunkLen; // nChunkLen * nChunkNum = SChannel's nBufSize
+	SAvailableChunk *astAvailableChunkList;
+} SPort;
+
+
+typedef struct _SChannel {
+	int nChannelIndex;
+	int nSampleSize;
+	void *pBuffer;
+	int nBufSize;
+	HThreadMutex hMutex; // channel global mutex
+	SPort stInputPort;
+	SPort stOutputPort;
+} SChannel;
 
 
 #ifdef __cplusplus
