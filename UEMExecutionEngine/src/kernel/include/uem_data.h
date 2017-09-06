27fd1aea7e439b867441dce8c683d9f82c680d46
chjej202 <chjej202@iris.snu.ac.kr>
2017-09-06 16:19:39 +0900

    An example of data creation code is added.
    - An example of data creation code is added.
    
    Signed-off-by: chjej202 <chjej202@iris.snu.ac.kr>

--- UEMExecutionEngine/src/kernel/include/uem_data.h
+++ UEMExecutionEngine/src/kernel/include/uem_data.h
@@ -270,9 +270,6 @@ typedef struct _SMappingSchedulingInfo {
 	int nLocalId;
 } SMappingSchedulingInfo;
 
-SMappingSchedulingInfo g_astMappingAndSchedulingInfo[] = {};
-
-
 void MatA_init();
 void MatA_go();
 void MatA_wrapup();
@@ -289,6 +286,87 @@ void Display_init();
 void Display_go();
 void Display_wrapup();
 
+#define CHANNEL_0_SIZE (3)
+#define CHANNEL_1_SIZE (3)
+#define CHANNEL_2_SIZE (36)
+
+char pChannel_0_buffer[CHANNEL_0_SIZE];
+char pChannel_1_buffer[CHANNEL_1_SIZE];
+char pChannel_2_buffer[CHANNEL_2_SIZE];
+
+
+SChunk *g_astChunk_sdf_matrix_channel_0_out[] = {
+	{
+		pChannel_0_buffer, // Chunk start pointer
+		pChannel_0_buffer, // Data start pointer
+		pChannel_0_buffer, // Data end pointer
+		0, // wWitten data length
+		0, // Available data number;
+		NULL, // Chunk mutex
+		NULL, // Chunk conditional variable
+	},
+};
+
+SChunk *g_astChunk_sdf_matrix_channel_0_in[] = {
+	{
+		pChannel_0_buffer, // Chunk start pointer
+		pChannel_0_buffer, // Data start pointer
+		pChannel_0_buffer, // Data end pointer
+		0, // Written data length
+		0, // Available data number (for broadcast loop)
+		NULL, // Chunk mutex
+		NULL, // Chunk conditional variable
+	},
+};
+
+SChunk *g_astChunk_sdf_matrix_channel_1_out[] = {
+	{
+		pChannel_1_buffer, // Chunk start pointer
+		pChannel_1_buffer, // Data start pointer
+		pChannel_1_buffer, // Data end pointer
+		0, // wWitten data length
+		0, // Available data number;
+		NULL, // Chunk mutex
+		NULL, // Chunk conditional variable
+	},
+};
+
+SChunk *g_astChunk_sdf_matrix_channel_1_in[] = {
+	{
+		pChannel_1_buffer, // Chunk start pointer
+		pChannel_1_buffer, // Data start pointer
+		pChannel_1_buffer, // Data end pointer
+		0, // Written data length
+		0, // Available data number (for broadcast loop)
+		NULL, // Chunk mutex
+		NULL, // Chunk conditional variable
+	},
+};
+
+SChunk *g_astChunk_sdf_matrix_channel_2_out[] = {
+	{
+		pChannel_2_buffer, // Chunk start pointer
+		pChannel_2_buffer, // Data start pointer
+		pChannel_2_buffer, // Data end pointer
+		0, // wWitten data length
+		0, // Available data number;
+		NULL, // Chunk mutex
+		NULL, // Chunk conditional variable
+	},
+};
+
+SChunk *g_astChunk_sdf_matrix_channel_2_in[] = {
+	{
+		pChannel_2_buffer, // Chunk start pointer
+		pChannel_2_buffer, // Data start pointer
+		pChannel_2_buffer, // Data end pointer
+		0, // Written data length
+		0, // Available data number (for broadcast loop)
+		NULL, // Chunk mutex
+		NULL, // Chunk conditional variable
+	},
+};
+
 SPortSampleRate g_astPortSampleRate_sdf_matrix_MatA_out[] = {
 	{ "Default", // Mode name
 	   1, // Sample rate
@@ -296,12 +374,46 @@ SPortSampleRate g_astPortSampleRate_sdf_matrix_MatA_out[] = {
 	},
 };
 
+
 SPortSampleRate g_astPortSampleRate_sdf_matrix_VecMul_in1[] = {
 	{ "Default", // Mode name
 	   1, // Sample rate
 	   1, // Available number of data
 	},
 };
+
+
+SPortSampleRate g_astPortSampleRate_sdf_matrix_MatB_out[] = {
+	{ "Default", // Mode name
+	   1, // Sample rate
+	   1, // Available number of data
+	},
+};
+
+
+SPortSampleRate g_astPortSampleRate_sdf_matrix_VecMul_in2[] = {
+	{ "Default", // Mode name
+	   1, // Sample rate
+	   1, // Available number of data
+	},
+};
+
+
+SPortSampleRate g_astPortSampleRate_sdf_matrix_VecMul_out[] = {
+	{ "Default", // Mode name
+	   1, // Sample rate
+	   1, // Available number of data
+	},
+};
+
+
+SPortSampleRate g_astPortSampleRate_sdf_matrix_Display_in[] = {
+	{ "Default", // Mode name
+	   9, // Sample rate
+	   1, // Available number of data
+	},
+};
+
 
 // ports which located inside a subgraph and port-mapped to outer graph
 //SPort g_astPortMapList[] = {
@@ -313,9 +425,9 @@ SChannel g_astChannels[] = {
 	{
 		0, // Channel ID
 		CHANNEL_TYPE_SHARED_MEMORY, // Channel type
-		NULL, // Channel buffer size
-		3, // Channel size
-		NULL, // Channel data size
+		pChannel_0_buffer, // Channel buffer pointer
+		CHANNEL_0_SIZE, // Channel size
+		pChannel_0_buffer, // Channel data start
 		0, // Channel data length
 		NULL, // Mutex
 		{
@@ -339,15 +451,102 @@ SChannel g_astChannels[] = {
 			NULL, // Pointer to Subgraph port
 		}, // Output port information
 		{
-			NULL,
-			1,
-			1,
-		}, // input chunk information
+			g_astChunk_sdf_matrix_channel_0_in, // Array of chunk
+			1, // Chunk number
+			1, // Chunk size
+		}, // Input chunk information
 		{
-			NULL,
-			1,
-			1,
-		}, // output chunk information
+			g_astChunk_sdf_matrix_channel_0_out, // Array of chunk
+			1, // Chunk number
+			1, // Chunk size
+		}, // Output chunk information
+		NULL, // Available chunk list
+		NULL, // Chunk list head
+		NULL, // Chunk list tail
+	},
+	{
+		1, // Channel ID
+		CHANNEL_TYPE_SHARED_MEMORY, // Channel type
+		pChannel_1_buffer, // Channel buffer pointer
+		CHANNEL_1_SIZE, // Channel size
+		pChannel_1_buffer, // Channel data start
+		0, // Channel data length
+		NULL, // Mutex
+		{
+			2, // Task ID
+			"in2", // Port name
+			PORT_SAMPLE_RATE_FIXED, // Port sample rate type
+			g_astPortSampleRate_sdf_matrix_VecMul_in2, // Array of sample rate list
+			1, // Array element number of sample rate list
+			12, // Sample size
+			PORT_TYPE_QUEUE, // Port type
+			NULL, // Pointer to Subgraph port
+		}, // Input port information
+		{
+			1, // Task ID
+			"out", // Port name
+			PORT_SAMPLE_RATE_FIXED, // Port sample rate type
+			g_astPortSampleRate_sdf_matrix_MatB_out, // Array of sample rate list
+			1, // Array element number of sample rate list
+			12, // Sample size
+			PORT_TYPE_QUEUE, // Port type
+			NULL, // Pointer to Subgraph port
+		}, // Output port information
+		{
+			g_astChunk_sdf_matrix_channel_1_in, // Array of chunk
+			1, // Chunk number
+			1, // Chunk size
+		}, // Input chunk information
+		{
+			g_astChunk_sdf_matrix_channel_1_out, // Array of chunk
+			1, // Chunk number
+			1, // Chunk size
+		}, // Output chunk information
+		NULL, // Available chunk list
+		NULL, // Chunk list head
+		NULL, // Chunk list tail
+	},
+	{
+		2, // Channel ID
+		CHANNEL_TYPE_SHARED_MEMORY, // Channel type
+		pChannel_2_buffer, // Channel buffer pointer
+		CHANNEL_2_SIZE, // Channel size
+		pChannel_2_buffer, // Channel data start
+		0, // Channel data length
+		NULL, // Mutex
+		{
+			3, // Task ID
+			"in", // Port name
+			PORT_SAMPLE_RATE_FIXED, // Port sample rate type
+			g_astPortSampleRate_sdf_matrix_Display_in, // Array of sample rate list
+			1, // Array element number of sample rate list
+			36, // Sample size
+			PORT_TYPE_QUEUE, // Port type
+			NULL, // Pointer to Subgraph port
+		}, // Input port information
+		{
+			2, // Task ID
+			"out", // Port name
+			PORT_SAMPLE_RATE_FIXED, // Port sample rate type
+			g_astPortSampleRate_sdf_matrix_VecMul_out, // Array of sample rate list
+			1, // Array element number of sample rate list
+			4, // Sample size
+			PORT_TYPE_QUEUE, // Port type
+			NULL, // Pointer to Subgraph port
+		}, // Output port information
+		{
+			g_astChunk_sdf_matrix_channel_2_in, // Array of chunk
+			1, // Chunk number
+			1, // Chunk size
+		}, // Input chunk information
+		{
+			g_astChunk_sdf_matrix_channel_2_out, // Array of chunk
+			1, // Chunk number
+			1, // Chunk size
+		}, // Output chunk information
+		NULL, // Available chunk list
+		NULL, // Chunk list head
+		NULL, // Chunk list tail
 	},
 };
 
@@ -427,6 +626,48 @@ STask g_astTopTasks[] = {
 };
 
 STaskGraph g_pstTopGraph[] = { g_astTopTasks, NULL };
+
+STaskIdToTaskMap g_astTaskIdToTask[] = {
+	{ 	0, // Task ID
+		g_astTopTasks[0] // Task structure pointer
+	},
+	{ 	1, // Task ID
+		g_astTopTasks[1] // Task structure pointer
+	},
+	{ 	2, // Task ID
+		g_astTopTasks[2] // Task structure pointer
+	},
+};
+
+SProcessor g_astProcessorInfo[] = {
+	{ 	0, // Processor ID
+		"i7_0", // Processor name
+		4, // Processor pool size
+	},
+};
+
+SMappingSchedulingInfo g_astMappingAndSchedulingInfo[] = {
+	{	TASK_TYPE_COMPUTATIONAL, // Task type
+		{ .nTaskId = 0 }, // Task ID or composite task information
+		0, // Processor ID
+		0, // Processor local ID
+	},
+	{	TASK_TYPE_COMPUTATIONAL, // Task type
+		{ .nTaskId = 1 }, // Task ID or composite task information
+		0, // Processor ID
+		1, // Processor local ID
+	},
+	{	TASK_TYPE_COMPUTATIONAL, // Task type
+		{ .nTaskId = 2 }, // Task ID or composite task information
+		0, // Processor ID
+		2, // Processor local ID
+	},
+	{	TASK_TYPE_COMPUTATIONAL, // Task type
+		{ .nTaskId = 3 }, // Task ID or composite task information
+		0, // Processor ID
+		3, // Processor local ID
+	},
+};
 
 
 #ifdef __cplusplus
