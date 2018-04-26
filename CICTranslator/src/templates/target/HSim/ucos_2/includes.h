7964fa31c38c75028cfe7a6ef97c21165b92cbaa
chjej202 <chjej202@iris.snu.ac.kr>
2018-04-26 15:18:31 +0900

    CIC Translator and DynamicOptimizer projects are removed.
    - CIC Translator and DynamicOptimizer projects are removed.
    (These projects will no longer be used)
    
    Signed-off-by: chjej202 <chjej202@iris.snu.ac.kr>

--- CICTranslator/src/templates/target/HSim/ucos_2/includes.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-#include "os_cpu.h"
-#include "os_cfg.h"
-#include "ucos_ii.h"
--- DynamicOptimizer/src/bufferOpt/internal/Param.java
+++
@@ -1,9 +0,0 @@
-package bufferOpt.internal;
-
-public class Param {
-	static boolean Verbose = false;
-	static boolean PREEMPTIVE = false;
-	static int PREEMPTION_COST = 100;
-	static boolean CYCLO_STATIC = true;
-	static boolean GA_PRIORITY_NODE = true; // false => instance º° priority
-}
